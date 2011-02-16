869c5dbff36a43efeab7fe6391b98322a23f69e1
sbaer <steve@mcneel.com>
2011-02-16 08:27:42 -0800

    added MonoDevelop debugging addin project

---
+++ md_debug/RhinoSoftDebuggerEngine.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace MonoDevelop.Debugger.Soft.Rhino
+{
+	public class RhinoSoftDebuggerEngine : MonoDevelop.Debugger.IDebuggerEngine
+	{
+		public string Id
+		{
+			get{ return "Mono.Debugger.Soft.Rhino"; }
+		}
+		
+		public string Name
+		{
+			get { return "Mono Soft Debugger for Rhino"; }
+		}
+		
+		public bool CanDebugCommand (Core.Execution.ExecutionCommand cmd)
+		{
+			return true;
+		}	
+		public Mono.Debugging.Client.DebuggerStartInfo CreateDebuggerStartInfo (Core.Execution.ExecutionCommand cmd)
+		{
+			return new RhinoDebuggerStartInfo("Rhino");
+		}
+
+		public Mono.Debugging.Client.DebuggerSession CreateSession ()
+		{
+			return new RhinoSoftDebuggerSession();
+		}
+
+		public Mono.Debugging.Client.ProcessInfo[] GetAttachableProcesses ()
+		{
+			return new Mono.Debugging.Client.ProcessInfo[0];
+		}
+	}
+	
+	
+	class RhinoDebuggerStartInfo : MonoDevelop.Debugger.Soft.RemoteDebuggerStartInfo
+	{
+		public RhinoDebuggerStartInfo(string appName)
+			: base(appName, System.Net.IPAddress.Loopback, 10000)
+		{
+		}
+	}
+}
+

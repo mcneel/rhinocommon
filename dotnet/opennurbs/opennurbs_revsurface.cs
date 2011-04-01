3ab9cb5b2ef7af898616e45204ffe1a38e1bebc2
sbaer <steve@mcneel.com>
2011-03-31 21:59:51 -0700

    Added line/polyline versions of RevSurface.Create

--- dotnet/opennurbs/opennurbs_revsurface.cs
+++ dotnet/opennurbs/opennurbs_revsurface.cs
@@ -5,6 +5,16 @@ namespace Rhino.Geometry
   public class RevSurface : Surface
   {
     #region static create functions
+
+    public static RevSurface Create(Curve revoluteCurve, Line axisOfRevolution, double startAngleRadians, double endAngleRadians)
+    {
+      IntPtr pConstCurve = revoluteCurve.ConstPointer();
+      IntPtr pRevSurface = UnsafeNativeMethods.ON_RevSurface_Create(pConstCurve, ref axisOfRevolution, startAngleRadians, endAngleRadians);
+      if (IntPtr.Zero == pRevSurface)
+        return null;
+      return new RevSurface(pRevSurface, null, null);
+    }
+
     /// <example>
     /// <code source='examples\vbnet\ex_addtruncatedcone.vb' lang='vbnet'/>
     /// <code source='examples\cs\ex_addtruncatedcone.cs' lang='cs'/>
@@ -14,13 +24,27 @@ namespace Rhino.Geometry
     {
       return Create(revoluteCurve, axisOfRevolution, 0, 2.0 * Math.PI);
     }
-    public static RevSurface Create(Curve revoluteCurve, Line axisOfRevolution, double startAngleRadians, double endAngleRadians)
+    public static RevSurface Create(Line revoluteLine, Line axisOfRevolution, double startAngleRadians, double endAngleRadians)
     {
-      IntPtr pConstCurve = revoluteCurve.ConstPointer();
-      IntPtr pRevSurface = UnsafeNativeMethods.ON_RevSurface_Create(pConstCurve, ref axisOfRevolution, startAngleRadians, endAngleRadians);
-      if (IntPtr.Zero == pRevSurface)
-        return null;
-      return new RevSurface(pRevSurface, null, null);
+      using (LineCurve lc = new LineCurve(revoluteLine))
+      {
+        return Create(lc, axisOfRevolution, startAngleRadians, endAngleRadians);
+      }
+    }
+    public static RevSurface Create(Line revoluteLine, Line axisOfRevolution)
+    {
+      return Create(revoluteLine, axisOfRevolution, 0, 2.0 * Math.PI);
+    }
+    public static RevSurface Create(Polyline revolutePolyline, Line axisOfRevolution, double startAngleRadians, double endAngleRadians)
+    {
+      using (PolylineCurve plc = new PolylineCurve(revolutePolyline))
+      {
+        return Create(plc, axisOfRevolution, startAngleRadians, endAngleRadians);
+      }
+    }
+    public static RevSurface Create(Polyline revolutePolyline, Line axisOfRevolution)
+    {
+      return Create(revolutePolyline, axisOfRevolution, 0, 2.0 * Math.PI);
     }
 
     public static RevSurface CreateFromCone(Cone cone)

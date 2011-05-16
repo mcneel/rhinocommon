06c8351e6c885d7276d734a13008719b97c57f84
sbaer <steve@mcneel.com>
2011-05-16 09:45:47 -0700

    Added display conduit helper class which simple wraps DisplayPipeline events. fixes #1

--- dotnet/rhino/rhinosdkdisplayconduit.cs
+++ dotnet/rhino/rhinosdkdisplayconduit.cs
@@ -1,2 +1,109 @@
-// Display conduit functionality has been moved to events on the Display class
-// see rhinosdkdeisplaypipeline.cs
+using System;
+
+namespace Rhino.Display
+{
+  public abstract class DisplayConduit
+  {
+    bool m_bEnabled = false;
+    protected DisplayConduit()
+    {
+    }
+
+    public bool Enabled
+    {
+      get
+      {
+        return m_bEnabled;
+      }
+      set
+      {
+        m_bEnabled = value;
+        if (m_bEnabled)
+        {
+          Type base_type = typeof(DisplayConduit);
+          Type t = this.GetType();
+
+          System.Reflection.MethodInfo mi = t.GetMethod("CalculateBoundingBox");
+          if( mi.DeclaringType != base_type )
+            DisplayPipeline.CalculateBoundingBox += _CalculateBoundingBox;
+
+          mi = t.GetMethod("DrawForeground");
+          if (mi.DeclaringType != base_type)
+            DisplayPipeline.DrawForeground += _DrawForeground;
+
+          mi = t.GetMethod("DrawOverlay");
+          if (mi.DeclaringType != base_type)
+            DisplayPipeline.DrawOverlay += _DrawOverlay;
+
+          mi = t.GetMethod("PostDrawObjects");
+          if (mi.DeclaringType != base_type)
+            DisplayPipeline.PostDrawObjects += _PostDrawObjects;
+
+          mi = t.GetMethod("PreDrawObjects");
+          if (mi.DeclaringType != base_type)
+            DisplayPipeline.PreDrawObjects += _PreDrawObjects;
+        }
+        else
+        {
+          DisplayPipeline.CalculateBoundingBox -= _CalculateBoundingBox;
+          DisplayPipeline.DrawForeground -= _DrawForeground;
+          DisplayPipeline.DrawOverlay -= _DrawOverlay;
+          DisplayPipeline.PostDrawObjects -= _PostDrawObjects;
+          DisplayPipeline.PreDrawObjects -= _PreDrawObjects;
+        }
+      }
+    }
+
+    private void _CalculateBoundingBox(object sender, CalculateBoundingBoxEventArgs e) { CalculateBoundingBox(e); }
+    private void _DrawForeground(object sender, DrawEventArgs e) { DrawForeground(e); }
+    private void _DrawOverlay(object sender, DrawEventArgs e)  { DrawOverlay(e); }
+    private void _PostDrawObjects(object sender, DrawEventArgs e) { PostDrawObjects(e); }
+    private void _PreDrawObjects(object sender, DrawEventArgs e) { PreDrawObjects(e); }
+
+
+    /// <summary>
+    /// Use this function to increase the bounding box of scene so it includes the
+    /// geometry that you plan to draw in the "Draw" virtual functions
+    /// </summary>
+    /// <param name="e"></param>
+    protected virtual void CalculateBoundingBox(CalculateBoundingBoxEventArgs e)
+    {
+    }
+
+    /// <summary>
+    /// Called before objects are been drawn. Depth writing and testing are on.
+    /// </summary>
+    /// <param name="e"></param>
+    protected virtual void PreDrawObjects(DrawEventArgs e)
+    {
+    }
+
+    /// <summary>
+    /// Called after all non-highlighted objects have been drawn. Depth writing and testing are
+    /// still turned on. If you want to draw without depth writing/testing, see DrawForeground
+    /// </summary>
+    /// <param name="e"></param>
+    protected virtual void PostDrawObjects(DrawEventArgs e)
+    {
+    }
+
+    /// <summary>
+    /// Called after all non-highlighted objects have been drawn and PostDrawObjects has been called.
+    /// Depth writing and testing are turned OFF. If you want to draw with depth writing/testing,
+    /// see PostDrawObjects
+    /// </summary>
+    /// <param name="e"></param>
+    protected virtual void DrawForeground(DrawEventArgs e)
+    {
+    }
+
+    /// <summary>
+    /// If Rhino is in a feedback mode, the draw overlay call allows for temporary geometry to be drawn on top of
+    /// everything in the scene. This is similar to the dynamic draw routine that occurs with custom get point.
+    /// </summary>
+    /// <param name="e"></param>
+    protected virtual void DrawOverlay(DrawEventArgs e)
+    {
+    }
+  }
+}

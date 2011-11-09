6865843214b0c26cecc25585f5cf155eb80c2e5e
piac <giulio@mcneel.com>
2011-11-09 18:09:17 +0100

    beam commented

--- dotnet/opennurbs/opennurbs_beam.cs
+++ dotnet/opennurbs/opennurbs_beam.cs
@@ -1,10 +1,13 @@
-#pragma warning disable 1591
 using System;
 using System.Runtime.Serialization;
 
 #if USING_V5_SDK
 namespace Rhino.Geometry
 {
+  /// <summary>
+  /// Represents an extrusion, or an object such as a beam or profiled element,
+  /// that can be represented by a profile curve and two miter planes at the extremes.
+  /// </summary>
   [Serializable]
   public class Extrusion : Surface, ISerializable
   {
@@ -19,7 +22,11 @@ namespace Rhino.Geometry
     }
     #endregion
 
-    // serialization constructor
+    /// <summary>
+    /// Protected serialization constructor.
+    /// </summary>
+    /// <param name="info">The serialization data.</param>
+    /// <param name="context">The serialization context stream.</param>
     protected Extrusion(SerializationInfo info, StreamingContext context)
       : base (info, context)
     {
@@ -56,6 +63,9 @@ namespace Rhino.Geometry
     }
     #endregion
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Extrusion"/> class.
+    /// </summary>
     public Extrusion()
     {
       IntPtr ptr = UnsafeNativeMethods.ON_Extrusion_New(IntPtr.Zero);
@@ -63,7 +73,7 @@ namespace Rhino.Geometry
     }
 
     /// <summary>
-    /// Build a Brep form of the extrusion. The outer profile is always the first face of the Brep.
+    /// Constructs a Brep form of the extrusion. The outer profile is always the first face of the Brep.
     /// If there are inner profiles, additional Brep faces are created for each profile. If the
     /// outer profile is closed, then end caps are added as the last two faces of the Brep.
     /// </summary>
@@ -71,7 +81,7 @@ namespace Rhino.Geometry
     /// If true and the profiles have kinks, then the faces corresponding to those profiles are split
     /// so they will be G1.
     /// </param>
-    /// <returns>A Brep with a similar shape like this extrustion or null</returns>
+    /// <returns>A Brep with a similar shape like this extrustion, or null on error.</returns>
     public Brep ToBrep(bool splitKinkyFaces)
     {
       IntPtr pConstThis = ConstPointer();
@@ -79,12 +89,23 @@ namespace Rhino.Geometry
       return GeometryBase.CreateGeometryHelper(pBrep, null) as Brep;
     }
     
+    /// <summary>
+    /// Allows to set the two points at the extremes and the up vector.
+    /// </summary>
+    /// <param name="a">The start point.</param>
+    /// <param name="b">The end point.</param>
+    /// <param name="up">The up vector.</param>
+    /// <returns>true if the operation succeeded; otherwise false.
+    /// Setting up=a-b will make the operation fail.</returns>
     public bool SetPathAndUp(Point3d a, Point3d b, Vector3d up)
     {
       IntPtr pThis = NonConstPointer();
       return UnsafeNativeMethods.ON_Extrusion_SetPathAndUp(pThis, a, b, up);
     }
 
+    /// <summary>
+    /// Gets the start point of the path.
+    /// </summary>
     public Point3d PathStart
     {
       get
@@ -95,6 +116,10 @@ namespace Rhino.Geometry
         return rc;
       }
     }
+
+    /// <summary>
+    /// Gets the end point of the path.
+    /// </summary>
     public Point3d PathEnd
     {
       get
@@ -105,6 +130,10 @@ namespace Rhino.Geometry
         return rc;
       }
     }
+
+    /// <summary>
+    /// Gets the up vector of the path
+    /// </summary>
     public Vector3d PathTangent
     {
       get
@@ -116,6 +145,9 @@ namespace Rhino.Geometry
       }
     }
 
+    /// <summary>
+    /// Gets or sets the normal of the miter plane at start.
+    /// </summary>
     public Vector3d MiterPlaneNormalAtStart
     {
       get
@@ -131,6 +163,10 @@ namespace Rhino.Geometry
         UnsafeNativeMethods.ON_Extrusion_SetMiterPlaneNormal(pThis, 0, value);
       }
     }
+
+    /// <summary>
+    /// Gets or sets the normal of the miter plane at the end.
+    /// </summary>
     public Vector3d MiterPlaneNormalAtEnd
     {
       get
@@ -146,6 +182,10 @@ namespace Rhino.Geometry
         UnsafeNativeMethods.ON_Extrusion_SetMiterPlaneNormal(pThis, 1, value);
       }
     }
+
+    /// <summary>
+    /// Returns a value indicating whether a miter plane at start is defined.
+    /// </summary>
     public bool IsMiteredAtStart
     {
       get
@@ -155,6 +195,10 @@ namespace Rhino.Geometry
         return (1 == rc || 3 == rc);
       }
     }
+
+    /// <summary>
+    /// Gets a value indicating whether a miter plane at the end is defined.
+    /// </summary>
     public bool IsMiteredAtEnd
     {
       get
@@ -165,6 +209,9 @@ namespace Rhino.Geometry
       }
     }
 
+    /// <summary>
+    /// Gets a value indicating whether there is no gap among all surfaces constructing this object.
+    /// </summary>
     public override bool IsSolid
     {
       get
@@ -174,6 +221,9 @@ namespace Rhino.Geometry
       }
     }
 
+    /// <summary>
+    /// Gets a value indicating whether the surface that fills the bottom profile is existing.
+    /// </summary>
     public bool IsCappedAtBottom
     {
       get
@@ -183,6 +233,10 @@ namespace Rhino.Geometry
         return (1 == rc || 3 == rc);
       }
     }
+
+    /// <summary>
+    /// Gets a value indicating whether the surface that fills the top profile is existing.
+    /// </summary>
     public bool IsCappedAtTop
     {
       get
@@ -193,6 +247,9 @@ namespace Rhino.Geometry
       }
     }
 
+    /// <summary>
+    /// Gets the amount of capping surfaces.
+    /// </summary>
     public int CapCount
     {
       get
@@ -203,13 +260,13 @@ namespace Rhino.Geometry
     }
 
     /// <summary>
-    /// Get the transformation that maps the xy profile curve to its 3d location.
+    /// Gets the transformation that maps the xy profile curve to its 3d location.
     /// </summary>
     /// <param name="s">
     /// 0.0 = starting profile
     /// 1.0 = ending profile
     /// </param>
-    /// <returns>Transformation on success. Invalid transform on failure</returns>
+    /// <returns>A Transformation. The transform is Invalid on failure</returns>
     public Transform GetProfileTransformation(double s)
     {
       Transform xform = new Transform();
@@ -221,13 +278,13 @@ namespace Rhino.Geometry
     }
 
     /// <summary>
-    /// Get the the 3d plane containing the profile curve at a normalized path parameter.
+    /// Gets the 3D plane containing the profile curve at a normalized path parameter.
     /// </summary>
     /// <param name="s">
     /// 0.0 = starting profile
     /// 1.0 = ending profile
     /// </param>
-    /// <returns>Plane on success. Invalid plane on failure</returns>
+    /// <returns>A plane. The plane is Invalid on failure.</returns>
     /// <remarks>
     ///  When no mitering is happening, GetPathPlane() and GetProfilePlane() return the same plane.
     /// </remarks>
@@ -241,13 +298,13 @@ namespace Rhino.Geometry
     }
 
     /// <summary>
-    /// Get the the 3d plane perpindicular to the path at a normalized path parameter.
+    /// Gets the 3D plane perpendicular to the path at a normalized path parameter.
     /// </summary>
     /// <param name="s">
     /// 0.0 = starting profile
     /// 1.0 = ending profile
     /// </param>
-    /// <returns>Plane on success. Invalid plane on failure</returns>
+    /// <returns>A plane. The plane is Invalid on failure.</returns>
     /// <remarks>
     ///  When no mitering is happening, GetPathPlane() and GetProfilePlane() return the same plane.
     /// </remarks>
@@ -282,7 +339,7 @@ namespace Rhino.Geometry
     }
 
     /// <summary>
-    /// Add an inner profile
+    /// Adds an inner profile.
     /// </summary>
     /// <param name="innerProfile">Closed curve in the XY plane or a 2d curve</param>
     /// <returns>True if the profile was set</returns>
@@ -294,7 +351,7 @@ namespace Rhino.Geometry
     }
 
     /// <summary>
-    /// Number of profile curves
+    /// Gets the amount of profile curves.
     /// </summary>
     public int ProfileCount
     {
@@ -310,7 +367,7 @@ namespace Rhino.Geometry
     //Profile
 
     /// <summary>
-    /// 
+    /// Gets a transversal isocurve of the extruded profile.
     /// </summary>
     /// <param name="profileIndex">
     /// 0 &lt;= profileIndex &lt; ProfileCount
@@ -329,6 +386,11 @@ namespace Rhino.Geometry
       return GeometryBase.CreateGeometryHelper(pCurve, null) as Curve;
     }
 
+    /// <summary>
+    /// Gets one of the profiles.
+    /// </summary>
+    /// <param name="ci">The index of this profile.</param>
+    /// <returns>The profile.</returns>
     public Curve Profile3d(ComponentIndex ci)
     {
       IntPtr pConstThis = ConstPointer();
@@ -336,6 +398,11 @@ namespace Rhino.Geometry
       return GeometryBase.CreateGeometryHelper(pCurve, null) as Curve;
     }
 
+    /// <summary>
+    /// Gets one of the longitudinal curves along the beam or extrusion.
+    /// </summary>
+    /// <param name="ci">The index of this profile.</param>
+    /// <returns>The profile.</returns>
     public Curve WallEdge(ComponentIndex ci)
     {
       IntPtr pConstThis = ConstPointer();
@@ -343,6 +410,11 @@ namespace Rhino.Geometry
       return GeometryBase.CreateGeometryHelper(pCurve, null) as Curve;
     }
 
+    /// <summary>
+    /// Gets one of the longitudinal surfaces of the extrusion.
+    /// </summary>
+    /// <param name="ci"></param>
+    /// <returns></returns>
     public Surface WallSurface(ComponentIndex ci)
     {
       IntPtr pConstThis = ConstPointer();
@@ -350,6 +422,10 @@ namespace Rhino.Geometry
       return GeometryBase.CreateGeometryHelper(pSurface, null) as Surface;
     }
 
+    /// <summary>
+    /// Gets the line-like curve that is the conceptual axis of the extrusion.
+    /// </summary>
+    /// <returns></returns>
     public LineCurve PathLineCurve()
     {
       IntPtr pConstThis = ConstPointer();
@@ -358,9 +434,9 @@ namespace Rhino.Geometry
     }
 
     /// <summary>
-    /// Get index of the profile curve with domain containing this paramter. 
+    /// Gets the index of the profile curve at a domain related to a parameter. 
     /// </summary>
-    /// <param name="profileParameter">Parameter on profile curve</param>
+    /// <param name="profileParameter">Parameter on profile curve.</param>
     /// <returns>
     /// -1 if profileParameter does not correspond to a point on the profile curve.
     /// When the profileParameter corresponds to the end of one profile and the

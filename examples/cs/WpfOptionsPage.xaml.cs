ee66b82ebd21213a206881558e7f0a304b9841d3
sbaer <steve@mcneel.com>
2011-11-30 14:37:26 -0800

    added samples

---
+++ examples/cs/WpfOptionsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace examples_cs
+{
+  /// <summary>
+  /// Interaction logic for WpfOptionsPage.xaml
+  /// </summary>
+  public partial class WpfOptionsPage : Page
+  {
+    public WpfOptionsPage()
+    {
+      InitializeComponent();
+    }
+
+    private void OnKeyDown(object sender, KeyEventArgs e)
+    {
+    }
+  }
+}

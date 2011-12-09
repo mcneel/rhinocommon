1ce2d7b9544a0221fd397c94cd1093b280d5e982
piac <giulio@mcneel.com>
2011-12-09 20:01:51 +0100

    NameSpaceDoc.cs comments start

--- dotnet/NameSpaceDoc.cs
+++ dotnet/NameSpaceDoc.cs
@@ -1,3 +1,42 @@
-using System;
-
 // This file is used for documenting all of the namespaces in RhinoCommon
+
+namespace Rhino
+{
+  /// <summary>
+  /// The Rhino namespace contains fundamental types that
+  /// define commonly-used value types and classes used in Rhino.
+  /// <para>Examples are the Rhino <see cref="RhinoDoc">document class</see>,
+  /// <see cref="RhinoMath">math class</see> and
+  /// <see cref="RhinoApp">application class</see>.</para>
+  /// </summary>
+  [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+  class NamespaceDoc
+  {
+  }
+}
+
+namespace Rhino.Geometry
+{
+  /// <summary>
+  /// The Geometry namespace contains geometric types used in Rhino.
+  /// <para>Examples are lines, curves, meshes and boundary representations.</para>
+  /// </summary>
+  [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+  class NamespaceDoc
+  {
+  }
+}
+
+namespace Rhino.DocObjects
+{
+  /// <summary>
+  /// The DocObjects namespace contains types that correspond to geometry
+  /// that is found in a document.
+  /// <para>Generally, these types reference an attribute and have a
+  /// distinctive ID.</para>
+  /// </summary>
+  [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+  class NamespaceDoc
+  {
+  }
+}

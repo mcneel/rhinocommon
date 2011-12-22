d5e2e7a24fe58907d8c2948e1650ad4b73564de1
piac <giulio@mcneel.com>
2011-12-22 23:11:13 +0100

    DocumentControlledException message set

--- dotnet/opennurbs/opennurbs_object.cs
+++ dotnet/opennurbs/opennurbs_object.cs
@@ -11,6 +11,11 @@ namespace Rhino.Runtime
   [Serializable]
   public class DocumentCollectedException : Exception
   {
+    /// <summary>
+    /// Initializes a new instance of the document controlled excetion class.
+    /// </summary>
+    public DocumentCollectedException() :
+      base("This object cannot be modified because it is controlled by a document.") { }
   }
 
   /// <summary>

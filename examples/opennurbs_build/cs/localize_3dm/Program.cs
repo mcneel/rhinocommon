c1a8ffffe4985689a8c0c818f010f414c7eb392c
Brian Gillespie <brian@mcneel.com>
2012-06-16 20:43:40 -0700

    Revamped Program.cs to allow both extract and localize options. The two possible calling methods are: localize_3dm.exe -action:extract -3dm:MyModel.3dm -xml:ExtractedStrings.xml
    localize_3dm.exe -action:localize -3dm:MyModel.3dm -xml:LocalizedStrings.xml -targetDir:PathToOutputFileDirectory
    
    localize takes a target ouptut directory instead of a output file because the output filename is embedded in the XML file so translators can translate the name.

--- examples/opennurbs_build/cs/localize_3dm/Program.cs
+++ examples/opennurbs_build/cs/localize_3dm/Program.cs
@@ -13,11 +13,11 @@ namespace localize_3dm
       if (args.Length < 1)
       {
         var exe_name = System.AppDomain.CurrentDomain.FriendlyName;
-        Console.WriteLine("Syntax: {0} -action:[extract|localize] -xml:XML -3dm:MODEL [-target3dm:Output]", exe_name);
+        Console.WriteLine("Syntax: {0} -action:[extract|localize] -xml:XML -3dm:MODEL [-targetDir:Output]", exe_name);
         return;
       }
 
-      string action = "", model = "", xml = "", target3dm = "";
+      string action = "", model = "", xml = "", targetDir = "";
 
       foreach (string arg in args)
       {
@@ -27,8 +27,8 @@ namespace localize_3dm
           model = arg.Substring("-3dm:".Length);
         else if (arg.StartsWith("-xml:", StringComparison.InvariantCultureIgnoreCase))
           xml = arg.Substring("-xml:".Length);
-        else if (arg.StartsWith("-target3dm:", StringComparison.InvariantCultureIgnoreCase))
-          target3dm = arg.Substring("-target3dm:".Length);
+        else if (arg.StartsWith("-targetDir:", StringComparison.InvariantCultureIgnoreCase))
+          targetDir = arg.Substring("-targetDir:".Length);
       }
 
       switch (action)
@@ -37,99 +37,50 @@ namespace localize_3dm
           Extract(xml, model);
           return;
         case "localize":
-          Localize(xml, model, target3dm);
+          Localize(xml, model, targetDir);
           return;
         default:
           return;
       }
-      
-      System.IO.TextWriter alternate_output_stream = null;
-      for (int i = 0; i < args.Length; i++)
-      {
-        string arg = args[i];
-        // check for -out or /out option
-        if (arg.StartsWith("-out:", StringComparison.InvariantCultureIgnoreCase) || arg.StartsWith("/out:", StringComparison.InvariantCultureIgnoreCase))
-        {
-          // change destination of dump file
-          string dump_file_name = arg.Substring(5).Trim();
-          alternate_output_stream = new System.IO.StreamWriter(dump_file_name);
-          Console.SetOut(alternate_output_stream);
-          continue;
-        }
-
-        string filename = arg;
-        Console.Out.WriteLine("OpenNURBS Archive File:  {0}", filename);
-        using (var file3dm = Rhino.FileIO.File3dm.Read(filename))
-        {
-          if (file3dm == null)
-          {
-            Console.Out.WriteLine("Unable to open file");
-            continue;
-          }
-
-          Console.Out.WriteLine("Layers:");
-          foreach (var layer in file3dm.Layers)
-          {
-            Console.Out.WriteLine("  {0} ({1}) color: {2}", layer.FullPath, layer.Name, layer.Color);
-          }
-
-          Console.Out.WriteLine("Objects:");
-          foreach (var obj in file3dm.Objects)
-          {
-            Console.Out.WriteLine("  {0} ({1}) {3}->{2}", obj.Attributes.ObjectId, obj.Attributes.Name, obj.Attributes.ObjectColor, obj.Attributes.ColorSource);
-          }
-
-          if (null != file3dm.Views)
-          {
-            Console.Out.WriteLine("Views:");
-            foreach (var view in file3dm.Views)
-            {
-              Console.Out.WriteLine("  {0}", view.Name);
-            }
-          }
-
-          if (null != file3dm.NamedViews)
-          {
-            Console.Out.WriteLine("Named Views:");
-            foreach (var view in file3dm.NamedViews)
-            {
-              Console.Out.WriteLine("  {0}", view.Name);
-            }
-          }
-
-          Console.Out.WriteLine("Notes:");
-          Console.Out.WriteLine(file3dm.Notes.Notes);
-
-        }
-      }
-
-      if (alternate_output_stream != null)
-      {
-        // close the text dump file
-        alternate_output_stream.Close();
-      }
     }
 
-    static void Localize(string xml, string sourceModel, string targetModel)
+    static void Localize(string xml, string sourceModel, string targetDir)
     {
       XmlDocument doc = new XmlDocument();
       doc.Load(xml);
+      var docElem = doc.DocumentElement;
 
       using (var file3dm = Rhino.FileIO.File3dm.Read(sourceModel))
       {
         foreach (var layer in file3dm.Layers)
         {
-          var path = string.Format("//RhinoModel/Layers/Layer/Name[@English='{0}']", layer.Name);
+          var path = string.Format("//RhinoModel/Layers/Layer[@English='{0}']", layer.Name);
           var nameNode = (XmlElement)doc.SelectSingleNode(path);
           if (nameNode != null)
             layer.Name = nameNode.GetAttribute("Localized");
         }
 
-        file3dm.Polish();
-        file3dm.Write(targetModel, 5);
-      }
+        foreach (var obj in file3dm.Objects)
+        {
+          var path = string.Format("//RhinoModel/Objects/Object[@English='{0}']", obj.Attributes.Name.Replace("'", "&apos;"));
+          var node = (XmlElement)doc.SelectSingleNode(path);
+          if (node != null)
+            obj.Attributes.Name = node.GetAttribute("Localized");
+        }
 
-      
+        var notesNode = (XmlElement)doc.SelectSingleNode("//RhinoModel/Notes");
+        if (notesNode != null)
+        {
+          file3dm.Notes.Notes = notesNode.InnerText;
+        }
+
+        string filename = docElem.GetAttribute("Filename");
+        if (string.IsNullOrEmpty(filename))
+          filename = System.IO.Path.GetFileName(sourceModel);
+
+        file3dm.Polish();
+        file3dm.Write(System.IO.Path.Combine(targetDir, filename), 5);
+      }
     }
 
     static void Extract(string xml, string model)
@@ -137,6 +88,15 @@ namespace localize_3dm
       XmlDocument doc = new XmlDocument();
       doc.AppendChild(doc.CreateXmlDeclaration("1.0", "utf-8", "yes"));
       XmlElement body = (XmlElement)doc.AppendChild(doc.CreateElement("RhinoModel"));
+      XmlElement filename = (XmlElement)body.AppendChild(doc.CreateElement("Filename"));
+      filename.SetAttribute("English", System.IO.Path.GetFileName(model));
+      filename.SetAttribute("Localized", System.IO.Path.GetFileName(model));
+
+      if (!System.IO.File.Exists(model))
+      {
+        Console.WriteLine("Error: File does not exist: " + model);
+        return;
+      }
 
       using (var file3dm = Rhino.FileIO.File3dm.Read(model))
       {
@@ -150,12 +110,8 @@ namespace localize_3dm
         foreach (var layer in file3dm.Layers)
         {
           XmlElement layerElem = (XmlElement)layers.AppendChild(doc.CreateElement("Layer"));
-          XmlElement layerNameElem = (XmlElement)layerElem.AppendChild(doc.CreateElement("Name"));
-          layerNameElem.SetAttribute("English", layer.Name);
-          layerNameElem.SetAttribute("Localized", layer.Name);
-          XmlElement layerPathElem = (XmlElement)layerElem.AppendChild(doc.CreateElement("FullPath"));
-          layerPathElem.SetAttribute("English", layer.FullPath);
-          layerPathElem.SetAttribute("Localized", layer.FullPath);
+          layerElem.SetAttribute("English", layer.Name);
+          layerElem.SetAttribute("Localized", layer.Name);
         }
 
         XmlNode objects = (XmlElement)body.AppendChild(doc.CreateElement("Objects"));
@@ -165,20 +121,18 @@ namespace localize_3dm
             continue;
           XmlElement objNode = (XmlElement)objects.AppendChild(doc.CreateElement("Object"));
           objNode.SetAttribute("ID", obj.Attributes.ObjectId.ToString());
-          XmlElement nameNode = (XmlElement)objNode.AppendChild(doc.CreateElement("Name"));
-          nameNode.SetAttribute("English", obj.Attributes.Name);
-          nameNode.SetAttribute("Localized", obj.Attributes.Name);
+          objNode.SetAttribute("English", obj.Attributes.Name);
+          objNode.SetAttribute("Localized", obj.Attributes.Name);
         }
 
         if (null != file3dm.Views)
         {
-          XmlNode views = (XmlElement)body.AppendChild(doc.CreateElement("Views"));
+          XmlNode views = (XmlElement)body.AppendChild(doc.CreateElement("Viewports"));
           foreach (var view in file3dm.Views)
           {
-            XmlElement viewNode = (XmlElement)views.AppendChild(doc.CreateElement("View"));
-            XmlElement nameNode = (XmlElement)viewNode.AppendChild(doc.CreateElement("Name"));
-            nameNode.SetAttribute("English", view.Name);
-            nameNode.SetAttribute("Localized", view.Name);
+            XmlElement viewNode = (XmlElement)views.AppendChild(doc.CreateElement("Viewport"));
+            viewNode.SetAttribute("English", view.Name);
+            viewNode.SetAttribute("Localized", view.Name);
           }
         }
 
@@ -188,9 +142,8 @@ namespace localize_3dm
         //  foreach (var view in file3dm.NamedViews)
         //  {
         //    XmlElement viewNode = (XmlElement)namedViews.AppendChild(doc.CreateElement("NamedView"));
-        //    XmlElement nameNode = (XmlElement)viewNode.AppendChild(doc.CreateElement("Name"));
-        //    nameNode.SetAttribute("English", view.Name);
-        //    nameNode.SetAttribute("Localized", view.Name);
+        //    viewNode.SetAttribute("English", view.Name);
+        //    viewNode.SetAttribute("Localized", view.Name);
         //  }
         //}
 

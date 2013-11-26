45d279009c0b15a33764ec6de7383f73918ed376
alain <alain@acormier.com>
2013-11-26 14:47:49 +0100

    update nuget icon and repackage and deploy

--- nuget/nupkg_x64/pushToNuGet.bat
+++ nuget/nupkg_x64/pushToNuGet.bat
@@ -1,4 +1,4 @@
 REM this will only work if you've previously run the command
 REM nuget setApiKey <api-key-for-your-account>
-nuget push Rhino3dmIO.dll-x64-Windows.5.1.30000.10.nupkg -NonInteractive
+nuget push Rhino3dmIO.dll-x64-Windows.5.1.30000.12.nupkg -NonInteractive
 

5a983a79e3504ca07e0205a9ee05a72a9a12fe51
alain <alain@acormier.com>
2013-12-31 10:28:30 +0100

    rhinocommon sample - singlecolorbackfaces - wip

---
+++ examples/cs/ex_singlecolorbackfaces.cs
@@ -0,0 +1,28 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Display;
+using System.Linq;
+using Rhino.Input.Custom;
+using Rhino.DocObjects;
+
+namespace examples_cs
+{
+  public class SingleColorBackfacesCommand : Command
+  {
+    public override string EnglishName { get { return "csSingleColorBackfaces"; } }
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      var display_mode_descs =
+        from dm in DisplayModeDescription.GetDisplayModes()
+        where dm.EnglishName == "Shaded"
+        select dm;
+
+      foreach (var dmd in display_mode_descs)
+      {
+        //RhinoApp.WriteLine(dmd.DisplayAttributes.);
+      }
+      return Result.Success;
+    }
+  }
+}

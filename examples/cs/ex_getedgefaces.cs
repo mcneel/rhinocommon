125c6d56f24ebe8d2893e49b0043668d1f9c5367
alain <alain@acormier.com>
2014-02-27 17:07:37 +0100

    RhinoCommon samples: sortpoints, getedgefaces

---
+++ examples/cs/ex_getedgefaces.cs
@@ -0,0 +1,35 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.DocObjects;
+using Rhino.Input;
+
+namespace examples_cs
+{
+  public class GetEdgeFacesCommand : Command
+  {
+    public override string EnglishName { get { return "csGetEdgeFaces"; } }
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      ObjRef obj_ref;
+      var rc = RhinoGet.GetOneObject(
+        "Select edge curve", false, ObjectType.EdgeFilter, out obj_ref);
+      if (rc != Result.Success)
+        return rc;
+      var edge = obj_ref.Edge();
+
+      var face_idxs = edge.AdjacentFaces();
+      var edge_owning_brep = edge.Brep;
+
+      foreach (var idx in face_idxs)
+      {
+        var face = edge_owning_brep.Faces[idx];
+        var face_copy = face.DuplicateFace(true);
+        var id = doc.Objects.AddBrep(face_copy);
+        doc.Objects.Find(id).Select(true);
+      }
+      doc.Views.Redraw();
+      return Result.Success;
+    }
+  }
+}
---
+++ examples/cs/ex_sortpoints.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+
+namespace examples_cs
+{
+  public class SortPointsCommand : Command
+  {
+    public override string EnglishName { get { return "csSortPoints"; } }
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      var points = new List<Point3d>
+      {
+        new Point3d(0, 0, 0),
+        new Point3d(0, 0, 1),
+        new Point3d(0, 1, 0),
+        new Point3d(0, 1, 1),
+        new Point3d(1, 0, 0),
+        new Point3d(1, 0, 1),
+        new Point3d(1, 1, 0),
+        new Point3d(1, 1, 1)
+      };
+
+      RhinoApp.WriteLine("Before sort ...");
+      foreach (var point in points)
+        RhinoApp.WriteLine("point: {0}", point);
+
+      var sorted_points = Point3d.SortAndCullPointList(points, doc.ModelAbsoluteTolerance);
+
+      RhinoApp.WriteLine("After sort ...");
+      foreach (var point in sorted_points)
+        RhinoApp.WriteLine("point: {0}", point);
+
+      doc.Objects.AddPoints(sorted_points);
+      doc.Views.Redraw();
+      return Result.Success;
+    }
+  }
+}
